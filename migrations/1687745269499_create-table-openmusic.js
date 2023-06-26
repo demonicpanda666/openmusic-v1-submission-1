d4cebe81d1b61bf5bb98ab984d6ee52f28ffb9ce
Wildan <akudia832@gmail.com>
2023-06-26 10:49:18 +0800

    add table

--- migrations/1687745269499_create-table-openmusic.js
+++ migrations/1687745269499_create-table-openmusic.js
@@ -2,6 +2,54 @@
 
 exports.shorthands = undefined;
 
-exports.up = pgm => {};
+exports.up = (pgm) => {
+  pgm.createTable('albums', {
+    album_id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+  pgm.createTable('songs', {
+    song_id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    genre: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    performer: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    duration: {
+      type: 'TEXT',
+      notNull: false,
+    },
+    album_id: {
+      type: 'TEXT',
+      notNull: false,
+    },
+  });
+};
 
-exports.down = pgm => {};
+exports.down = (pgm) => {
+  pgm.dropTable('albums');
+  pgm.dropTable('songs');
+};
